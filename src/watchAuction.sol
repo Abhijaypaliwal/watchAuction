c3ff203826295165a8a64539ae007607195a106f
Abhijay Paliwal <abhijaypaliwal@infograins.com>
2023-09-14 15:10:17 +0530

    new commit

--- src/watchAuction.sol
+++ src/watchAuction.sol
@@ -5,55 +5,75 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 import {Test, console} from "forge-std/Test.sol";
 
-
-contract getEMI  {
+contract getEMI {
     uint principal;
     uint interestRate;
     uint time;
     address borrower;
     address[] investors;
     address debtTokenContract;
-
+    uint EMILeft; // months of EMI to be paid
+    uint penalty; // penalty paid by borrower for late payment, in % per day
+    uint public constant thirtyDayEpoch = 2629743;
+    uint public nextEpochToPay = 2629743 + block.timestamp;
     constructor(
         uint _principal,
         uint _interestRate,
         uint _time,
         address _borrower,
-        address[]  memory _investors,
-        address _debtTokenContract 
+        address[] memory _investors,
+        address _debtTokenContract
     ) {
         principal = _principal;
         interestRate = _interestRate * 1000;
         time = _time;
         borrower = _borrower;
-        _debtTokenContract = debtTokenContract; 
-       // investors = _investors;
+        _debtTokenContract = debtTokenContract;
+        investors = _investors;
+        EMILeft = _time;
     }
 
     modifier onlyBorrower() {
-        require(
-            msg.sender == borrower,
-            "only borrower can call this function"
-        );
+        require(msg.sender == borrower, "only borrower can call this function");
         _;
     }
 
-    function calcEMI(uint principal, uint rate, uint timeinMos) public pure returns(uint) {
-        uint EMI = (principal + (principal * rate * timeinMos)/1200)/12 ;
+    function calcEMI() public view returns (uint) {
+        //@dev note that time is in months and calculation is done via simple interest
+        uint EMI = (principal + (principal * interestRate * time) / 1200) / 12;
         return EMI;
     }
 
-    function transferFunds() public payable returns(bool) {
+    // @dev note that transfer function is build to support ETH currently
+    function transferFunds() public payable returns (bool) {
         // use output of calculateEMI
-        require(msg.value == calcEMI(principal, interestRate, time), "EMI AND MSG.VALUE DOES NOT MATCH");
+        require(msg.value == calcEMI(), "EMI AND MSG.VALUE DOES NOT MATCH");
+        require(EMILeft > 0, "NO EMI IS LEFT");
+        uint EMIToPay;
+        if (block.timestamp < nextEpochToPay) 
+        {
+            EMIToPay = calcEMI();
+        }
+        else {
+            uint daysElapsed = (block.timestamp - nextEpochToPay) / 86400 ; 
+            
+        }
+        uint totalSupplyDebtToken = getInvestorProposal(debtTokenContract).totalSupply();
 
-        for(uint i = 0; i <= investors.length ; i++) {
-            uint _tokenBal = getInvestorProposal(debtTokenContract).balanceOf(investors[i]);
+        for (uint i = 0; i <= investors.length; i++) {
+            uint _tokenBal = getInvestorProposal(debtTokenContract).balanceOf(
+                investors[i]
+            );
             (bool sent, ) = investors[i].call{value: _tokenBal}("");
             require(sent, "Failed to send Ether");
+            if (sent) EMILeft -=1;
+
         }
+        return true;
+        
     }
 }
+
 contract getInvestorProposal is ERC20, Test {
     struct itemDetails {
         string itemName;
@@ -219,6 +239,9 @@ contract getInvestorProposal is ERC20, Test {
         return true;
     }
 }
+
+// Auction watch contract
+// Author: Abhijay Paliwal
 
 contract AuctionWatch is ERC721 {
     struct itemDetails {
